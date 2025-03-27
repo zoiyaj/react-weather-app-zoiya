01d19538752fa28603525aa9daedbb6166a24d50
zoiyaj <z.jesani@gmail.com>
2025-03-27 10:06:44 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello, my Name is Zoiya</div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
 
-export default App;
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.delac.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Matt Delac
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
